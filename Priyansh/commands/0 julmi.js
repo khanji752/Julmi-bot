ffe3fa2ac508be721403a499bcd1883cfff541da
khanji752 <khanji75298@gmail.com>
2025-10-04 17:15:55 +0500

    Update 0 julmi.js

--- Priyansh/commands/0 julmi.js
+++ Priyansh/commands/0 julmi.js
@@ -14,10 +14,11 @@ module.exports.config = {
 }
 
 module.exports.run = async function({ api, args, Users, event}) {
-var mention = Object.keys(event.mentions)[0];
+var mention = Object.keys(event.mentions)[0];Naaina
 
-let name =  event.mentions[mention];
+let name =  event.mentions[mention];Naaina;)
   var arraytag = [];
       arraytag.push({id: mention});
   var a = function (a) { api.sendMessage(a, event.threadID); }
-a("🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊 :-👇👇https://www.facebook.com/profile.php?id=100090277345439-😘😘_𝗡𝗔𝗜𝗜𝗡𝗔_𝗤𝗨𝗘𝗘𝗡__?𝗜 𝗔𝗠 𝗡𝗔𝗔𝗜𝗡𝗔 𝗖𝗔𝗥𝗧𝗢𝗢𝗡 𝗕𝗢𝗧𝗧==
+
+🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰\n\n (─━━◉❖NAINA❖◉━━─)(•◡•)\n\n𝐀𝐠𝐞 : 23\n\n𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 :-D3V1 )\n\n𝐅𝐫𝐨𝐦 : INDIA)\n\n𝐒𝐭𝐮𝐝𝐲 : 𝐂𝐨𝐦𝐩𝐮𝐭𝐞𝐫 𝐏𝐫𝐨𝐠𝐫𝐚𝐦𝐦𝐢𝐧𝐠\n\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :-👇👇https://www.facebook.com/profile.php?id=100090277345439-😘😘_𝗡𝗔𝗜𝗜𝗡𝗔_𝗤𝗨𝗘𝗘𝗡__?𝗜 𝗔𝗠 𝗡𝗔𝗔𝗜𝗡𝗔 𝗖𝗔𝗥𝗧𝗢𝗢𝗡 𝗕𝗢𝗧𝗧==");
