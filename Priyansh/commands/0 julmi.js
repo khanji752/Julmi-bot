f628cbeca253fbe051b3dc91de039af2a5f7a40e
khanji752 <khanji75298@gmail.com>
2025-10-04 14:16:53 +0500

    Update 0 julmi.js

--- Priyansh/commands/0 julmi.js
+++ Priyansh/commands/0 julmi.js
@@ -20,7 +20,7 @@ let name =  event.mentions[mention];
   var arraytag = [];
       arraytag.push({id: mention});
   var a = function (a) { api.sendMessage(a, event.threadID); }
-a("🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰\n\n (─━━◉❖𝗕𝗔𝗗𝗦𝗛𝗔𝗛❖◉━━─)(•◡•)\n\n𝐀𝐠𝐞 : 23\n\n𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 :-🄿🅁🄴🄴🅃(ɴᴀᴅᴀɴ ʙᴄʜɪ)\n\n𝐅𝐫𝐨𝐦 : 𝐉𝐈𝐍𝐃(𝐇𝐀𝐑𝐘𝐀𝐍𝐀)\n\n𝐒𝐭𝐮𝐝𝐲 : 𝐂𝐨𝐦𝐩𝐮𝐭𝐞𝐫 𝐏𝐫𝐨𝐠𝐫𝐚𝐦𝐦𝐢𝐧𝐠\n\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :-👇👇https://www.facebook.com/profile.php?id=100090277345439-😘😘_𝗡𝗔𝗜𝗜𝗡𝗔_𝗤𝗨𝗘𝗘𝗡__?igshid=OGQ5ZDc2ODk2ZA==");
+a("🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰\n\n (─━━◉❖𝗕𝗔𝗗𝗦𝗛𝗔𝗛❖◉━━─)(•◡•)\n\n𝐀𝐠𝐞 : 23\n\n𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 :-🄿🅁🄴🄴🅃(ɴᴀᴅᴀɴ ʙᴄʜɪ)\n\n𝐅𝐫𝐨𝐦 : 𝐉𝐈𝐍𝐃(𝐇𝐀𝐑𝐘𝐀𝐍𝐀)\n\n𝐒𝐭𝐮𝐝𝐲 : 𝐂𝐨𝐦𝐩𝐮𝐭𝐞𝐫 𝐏𝐫𝐨𝐠𝐫𝐚𝐦𝐦𝐢𝐧𝐠\n\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :-👇👇https://www.facebook.com/profile.php?id=100090277345439-😘😘_𝗡𝗔𝗜𝗜𝗡𝗔_𝗤𝗨𝗘𝗘𝗡__?𝗜𝗜 𝗔𝗔𝗠𝗠 𝗡𝗔𝗔𝗜𝗡𝗔 𝗖𝗔𝗥𝗧𝗢𝗢𝗡 𝗕𝗢𝗧𝗧==");
 
 
 }
